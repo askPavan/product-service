ebfe70681af83653f3b28c18e31de757f1f1a2fc
PavanKumar <pavankumar.ambraji@gmail.com>
2022-12-05 23:21:13 +0530

    Implemented Service for Testing Spring cloud Load Balancer

---
+++ src/main/java/com/productservice/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.productservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+	
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping("/products/{productNo}")
+	public ResponseEntity<String> getProducts(@PathVariable("productNo") String productNo){
+		System.out.println("Fetching products....");
+		return restTemplate.getForEntity("http://eureka-discovery-client-app/api/products/"+productNo, String.class);
+	}
+}
